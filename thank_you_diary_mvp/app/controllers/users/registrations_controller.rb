75c3f9d307aa96863854eae1f116e38e383714a3
kejimonte <e1601130kk113095@gmail.com>
2026-03-15 15:52:58 +0900

    change message2

--- thank_you_diary_mvp/app/controllers/users/registrations_controller.rb
+++
@@ -1,9 +0,0 @@
-#class Users::RegistrationsController < Devise::RegistrationsController
-#  def create
-#    super do |resource|
-#      if resource.persisted?
-#        sign_in(resource)
-#      end
-#    end
-#  end
-#end
--- thank_you_diary_mvp/config/routes.rb
+++ thank_you_diary_mvp/config/routes.rb
@@ -22,7 +22,6 @@ Rails.application.routes.draw do
   # root "home#index"
   devise_for :users, controllers:
   {
-    #registrations: "users/registrations"
     omniauth_callbacks: "users/omniauth_callbacks"
   }
 
